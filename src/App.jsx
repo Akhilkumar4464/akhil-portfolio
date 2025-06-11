0910c4c4ebf1646dee7cf52d28d1d1b67ce6aa96
Akhilkumar4464 <niteshsharma4464@gmail.com>
2025-06-11 17:48:38 +0530

    '''

--- src/App.jsx
+++ src/App.jsx
@@ -8,17 +8,13 @@ import ContactForm from "./components/ContactForm";
 
 function App() {
   return (
-    
-    
     <Router>
       <div className="min-h-screen md:w-screen flex flex-col">
-        <Navbar  className="h-[20%] "/>
+        <Navbar className="h-[20%]" />
         <main className="flex-grow h-[80%]">
-            <AppRoutes/>
-         
+          <AppRoutes />
         </main>
-       
-        <Footer className="h-[20%] "/>
+        <Footer className="h-[20%]" />
       </div>
     </Router>
   );
